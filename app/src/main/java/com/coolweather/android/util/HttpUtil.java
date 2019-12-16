693ec229359236507c8ba90306dc2a8b88015a13
Tony <tony@gmail.com>
2019-12-16 14:48:42 +0800

    新增切换城市和手动更新天气的功能

--- app/src/main/java/com/coolweather/android/util/HttpUtil.java
+++ app/src/main/java/com/coolweather/android/util/HttpUtil.java
@@ -1,4 +1,14 @@
 package com.coolweather.android.util;
 
+import java.net.URL;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
 public class HttpUtil {
+    public static void sendOkHttpRequest(String address, okhttp3.Callback callback){
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(address).build();
+        client.newCall(request).enqueue(callback);
+    }
 }
